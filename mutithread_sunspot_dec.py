638955eae6737f83fef54c05e1ce8d6ee12d0c26
wen-jams <jimmywen74@gmail.com>
2024-06-18 13:28:56 -0700

    implement kmeans

--- mutithread_sunspot_dec.py
+++ mutithread_sunspot_dec.py
@@ -2,14 +2,14 @@ import concurrent.futures
 from segment import segment_core
 
 
-def process_images(image_list, scale_factor, max_workers=4):
+def process_images(image_list, feature, max_workers=4):
     with concurrent.futures.ThreadPoolExecutor(max_workers=max_workers) as executor:
         futures = []
         for image in image_list:
             png_path = f"{image}.fits.png"
             fits_path = f"{image}.fits"
             # Schedule the execution of the main function
-            future = executor.submit(segment_core, png_path, fits_path, scale_factor)
+            future = executor.submit(segment_core, fits_path, feature=feature)
             futures.append(future)
 
         # Waiting for all futures to complete
@@ -26,4 +26,4 @@ image_files = [
     "test_res/hmi.in_45s.20150512_220000_TAI.2.continuum"
 ]
 
-process_images(image_files, scale_factor=2)
+process_images(image_files, feature="penumbrae", max_workers=2)
--- segment.py
+++ segment.py
@@ -1,10 +1,11 @@
 import os
 import subprocess
 import numpy as np
+import cv2
 import skimage as ski
+from scipy import ndimage as ndi
 from PIL.Image import Image
 from astropy.io import fits
-from skimage import measure
 
 Image.MAX_IMAGE_PIXELS = None
 
@@ -17,186 +18,273 @@ def open_fits_image(image_path, image_data_header_location):
     hdu_list.info()
     image_data = hdu_list[image_data_header_location].data
     return image_data
-
-
-# splits a given square image into a collection of square tiles
-def reshape_split(image: np.ndarray, kernel_size: int):
-    img_height, img_width = image.shape
-    tile_height = tile_width = kernel_size  # tile must be square
-
-    tiled_array = image.reshape(img_height // tile_height,
-                                tile_height,
-                                img_width // tile_width,
-                                tile_width)
-    tiled_array = tiled_array.swapaxes(1, 2)
-    return tiled_array
-
-
-# reads the given image into an array
-def read_image(img_name):
-    print("Attempting to read image...")
-    img_ext = img_name.split(".")[-1]
-    if img_ext == 'fits':
-        return open_fits_image(img_name, 0)
-    else:
-        return ski.img_as_float(ski.io.imread(img_name, as_gray=True))
     
 
-# prepare image for thresholding
+# prepare image for segmentation
 def preprocess(image):
-    # replace black & near-black values with mean value of a central tile
-    # this assumes such values only lie outside the disk
-    height, width = image.shape
-    y_mid = height//2
-    x_mid = width//2
-    central_tile = image.copy()
-    ratio = 40 #ratio of full img width to tile width
-    central_tile = central_tile[y_mid-height//ratio:y_mid+height//ratio, x_mid-width//ratio:x_mid+width//ratio]
-    processed = image.copy()
-    processed[np.where(processed < 0.02)] = np.mean(central_tile)
-    return processed
-
-
-# Upscale image using Real-ESRGAN: https://github.com/xinntao/Real-ESRGAN
-# Default scale factor is 4
-def upscale_image(input_path, output_path, scale_factor=4):
-    command = [
-        './realesrgan-ncnn-vulkan',
-        '-i', input_path,
-        '-o', output_path,
-        '-s', str(scale_factor)
-    ]
-
-    # Execute the command
-    try:
-        result = subprocess.run(command, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        print("Upscaling successful:", result.stdout)
-    except subprocess.CalledProcessError as e:
-        print("Error during upscaling:", e.stderr)
-
-
-# splits a given image into tiles, then applies Otsu's thresholding method
-def thresh_by_tile(base_image, tile_size=512):
-    # split image into square tiles of length tile_size
-    base_size = base_image.shape[0]  # assuming image is square
-    divs = base_size // tile_size
-    num_tiles = int(divs ** 2)  # number of tiles
-    tiles = reshape_split(base_image, tile_size)
-
-    # flatten tile array along one dimension
-    tiles = tiles.reshape(num_tiles, tile_size, tile_size)
-
-    # apply threshold to each tile
-    thresholded = np.zeros_like(tiles, dtype='bool')
-    for i in range(0, tiles.shape[0]):
-        img = tiles[i]
-        if img.any():
-            ints = img.flatten()
-            # mask out black values
-            ints = ints[ints != 0]
-            mean = np.mean(ints)
-            std = np.std(ints)
-            # check if there's anything significantly darker than the background
-            if len(ints[ints < mean - 7 * std]) > 64:
-                # threshold with Otsu's method
-                thresh = ski.filters.threshold_otsu(img[img != 0])
-                thresh = 0.90 * thresh
-                otsu = img < thresh
-                thresholded[i] = otsu
-
-                # # morphological segmentation using Chan-Vese
-                # img = ski.img_as_float(img)
-                # cv = ski.segmentation.chan_vese(img, mu=0.01, lambda1=1, lambda2=1, tol=1e-5,
-                #                max_num_iter=1000, dt=1, init_level_set="checkerboard",
-                #                extended_output=True)
-                # thresholded[i] = cv[0]
-            else:
-                thresholded[i] = np.zeros_like(img, dtype='bool')
-        else:
-            thresholded[i] = np.zeros_like(img, dtype='bool')
-
-    # reassemble thresholded tiles into grid
-    thresholded = thresholded.reshape(divs, divs, tile_size, tile_size)
-    # stitch them together
-    stitched = thresholded.swapaxes(1, 2).reshape(base_image.shape)
-    return stitched
-
-
-def find_objects(image):
-    # remove small elements with morphological opening
-    footprint = ski.morphology.square(3)
-    closed = ski.morphology.binary_closing(image, footprint)
-    opened = ski.morphology.binary_opening(closed, footprint)
-
-    # remove artifacts connected to image border
-    cleared = ski.segmentation.clear_border(opened, bgval=0)
-
-    # remove artifacts on disk edge
-    nrows, ncols = cleared.shape
+    # replace off-disk pixels with mean value of a central tile
+    nrows, ncols = image.shape
     row, col = np.ogrid[:nrows, :ncols]
-    cnt_row, cnt_col = nrows / 2, ncols / 2
+    cnt_row, cnt_col = nrows // 2, ncols // 2
+    
+    central_tile = image.copy()
+    ratio = 40 # ratio of full img width to tile width
+    central_tile = central_tile[cnt_row-nrows//ratio:cnt_row+nrows//ratio, cnt_col-ncols//ratio:cnt_col+ncols//ratio]
+    
     # mask out pixels that lie outside of a disk with radius 91% of img size
-    outer_disk_mask = ((row - cnt_row)**2 + (col - cnt_col)**2 >
-                    (nrows / 2 * 0.91)**2)
-    cleared[outer_disk_mask] = 0
-
-    # label image regions
-    labeled = ski.measure.label(cleared)
-    return labeled
+    outer_disk_mask = ((row - cnt_row)**2 + (col - cnt_col)**2 > (nrows / 2 * 0.91)**2)
+    
+    image[outer_disk_mask] = np.mean(central_tile)
+    return image
 
 
-def segment_core(image_path, fits_path, scale_factor=4, tile_size=2048):
+def find_rois(image, num_stdevs=9, padding=60):
+    """
+    Finds the darkest pixels in an image and isolates the surrounding region of interest. 
+
+    The dark pixels are chosen by the number of standard deviations they are below 
+    the global mean intensity. A square region whose width is determined by the padding
+    parameter is then drawn around each chosen pixel.
+
+    NOTE: in general, more padding works better for segmenting images with highly variable backgrounds, 
+    as more objects will be grouped in the same region of interest and the background is "smoothed out".
+
+    Parameters
+    ----------
+    image: ndarray
+        The input image.
+
+    num_stdevs: int, optional
+        The number of standard deviations below the mean intensity. Pixels darker than this
+        will be used to locate regions of interest.
+
+    padding: int, optional
+        The amount of padding. This should be chosen such that the objects to be segmented 
+        are not touching the edges of the region of interest.
+
+    Returns
+    -------
+    regions: ndarray
+        A copy of the input image, but with every value outside the regions of interest 
+        set to zero. 
+    """
+    image = ski.util.img_as_float(image)
+    vals = image.flatten()
+    mean = np.mean(vals)
+    std = np.std(vals)
+    markers = np.zeros_like(image)
+    regions = np.zeros_like(image)
+    min_idxs = np.argwhere(image < mean - num_stdevs*std)
+    for idx in min_idxs:
+        # plot min idxs as markers
+        markers[idx[0], idx[1]] = image[idx[0], idx[1]]
+        # expand by padding a box surrounding the marker
+        box = [idx[0]-padding, idx[1]-padding, idx[0]+padding-1, idx[1]+padding-1]
+        regions[box[0]:box[2], box[1]:box[3]] = image[box[0]:box[2], box[1]:box[3]]
+    return regions
+
+
+# apply K-means clustering with an optional gaussian blur
+def kmeans(image, K, blur_strength=1, return_info=False):
+    """
+    Apply K-means clustering with an optional gaussian blur.
+
+    Uses OpenCV's implementation of K-means to bin intensity values in an image.
+    Noisy images may benefit from the optional Gaussian blur.
+
+    NOTE: K-means clustering is an unsupervised learning algorithm, so its results
+    may not be exactly reproducible. The `cv2.KMEANS_PP_CENTERS` flag should mitigate
+    this and is enabled by default. Refer to OpenCV documentation for more info.
+
+    Parameters
+    ----------
+    image: ndarray
+        The input image.
+
+    K: int
+        The number of bins (clusters).
+
+    blur_strength: int, optional
+        The width of the Gaussian kernel used for blurring. Default: 1 (no blur applied)
+
+    return_info: bool, optional
+        Whether to return additional outputs from the clustering algorithm. Default: False
+
+    Returns
+    -------
+    compactness: float
+        A measure of how closely the member values of each cluster match its central value. 
+        The K-means algorithm seeks to maximize this value. Only returned if `return_info` is True.
+
+    labels: ndarray
+        An array of labels for each value in the input image. The labels are indices for 
+        the array of cluster centers. Only returned if `return_info` is True.
+
+    centers: ndarray
+        An array of cluster centers, where each element represents the cluster's central value. 
+        Only returned if `return_info` is True.
+
+    result: ndarray
+        The resulting image, where each pixel is replaced by the central value of the cluster it
+        belongs to. 
+    """
+    if blur_strength > 1:
+        image = cv2.GaussianBlur(np.float32(image), (blur_strength,blur_strength), 0)
+    else:
+        image = np.float32(image)
+    Z = image.reshape(-1)
+    
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0) 
+    flags = cv2.KMEANS_PP_CENTERS
+    
+    # apply K-means algorithm
+    compactness, labels, centers, = cv2.kmeans(Z, K, None, criteria, 10, flags)
+
+    # paint each pixel in array with its labeled center value
+    painted_array = centers[labels.flatten()]
+    result = painted_array.reshape(image.shape)
+    
+    if return_info:
+        return compactness, labels, centers, result
+    else:
+        return result
+
+
+def clear_bg(clustered_img, bwidth=10, bg_min_count=50, return_vals=False):
+    """
+    Clears background of a k-clustered image, assuming border pixels are not objects of interest.
+
+    The pixel values on the image edges are considered background and removed (set to zero).
+    The objects of interest are assumed to be centered, substantially brighter/darker than 
+    the background, and sufficiently padded by background pixels. 
+    
+    To account for features that may lie near the edge, pixel values must meet a minimum number 
+    of occurences to be considered background.
+
+    Parameters
+    ----------
+    clustered_img: ndarray
+        The image to be cleared. Assumed to be processed with K-means clustering.
+
+    bwidth: int, optional
+        Width of the border in pixels. The shape of the border will be rectangular. Default: 10
+
+    bg_min_count: int, optional
+        Minimum count required for a pixel value to count as background. Default: 50
+
+    return_vals: bool, optional
+        Whether to return the values and number of occurences of pixels found in the border.
+
+    Returns
+    -------
+    cleared: ndarray
+        The image with background removed.
+
+    vals: tuple(ndarray)
+        A tuple with an array of unique pixel values within the border, followed by
+        an array of counts for each value.
+
+    """
+    nrows, ncols = clustered_img.shape
+    border = np.copy(clustered_img)
+    border[bwidth:nrows-bwidth, bwidth:ncols-bwidth] = 0
+    S = clustered_img.flatten()
+    unique_vals, counts = np.unique(border, return_counts=True)
+    unique_vals = unique_vals[np.where(counts > bg_min_count)]
+    S[np.argwhere(np.in1d(S, unique_vals))] = 0
+    cleared = S.reshape(clustered_img.shape)
+    if return_vals:
+        return cleared, (unique_vals, counts)
+    else:
+        return cleared
+
+
+def binarize_features(cleared_img, feature='penumbrae'):
+    """
+    Binarizes the cleared, k-clustered image with either umbrae or penumbrae as the target.
+
+    If "penumbrae" is chosen, sets the lightest pixels to 1 and all others to 0.
+
+    If "umbrae" is chosen, sets the darkest nonzero pixels to 1 and all others to 0.
+
+    Parameters
+    ----------
+    cleared_img: ndarray
+        The image to binarize. the background pixels should be 0.
+
+    feature: str, optional
+        The target feature, either penumbrae or umbrae. 
+        Umbrae are assumed to be the darker of the two.
+
+    Returns
+    -------
+    binarized: ndarray
+        The binarized image. 
+    """
+    unique_vals = np.unique(cleared_img)
+    binarized = np.copy(cleared_img)
+    if feature == 'penumbrae':
+        binarized[binarized > 0] = 1
+    elif feature == 'umbrae':
+        binarized[binarized > np.min(unique_vals[np.nonzero(unique_vals)])] = 0
+        binarized[binarized == np.min(unique_vals[np.nonzero(unique_vals)])] = 1
+    else:
+        raise Exception("Detection of this feature is not supported.")
+    return binarized
+
+def segment_core(fits_path, image_path=None, feature="penumbrae"):
     # check if file exists
-    if not os.path.exists(image_path):
-        raise FileNotFoundError(f"Image file {image_path} not found.")
     if not os.path.exists(fits_path):
         raise FileNotFoundError(f"FITS file {fits_path} not found.")
+    if image_path and not os.path.exists(image_path):
+        raise FileNotFoundError(f"Image file {image_path} not found.")
+    
+    #extract the date from the fits file path
+    date = fits_path.split('/')[-1].split('.')[2]
 
-    #extract the date from the image path
-    date = image_path.split('/')[-1].split('.')[2]
-
-    # read image and upscale if necessary
-    if scale_factor > 1:
-        try:
-            base_image_path = 'upx' + str(scale_factor) + '_' + date + '_.png'
-            upscale_image(image_path, base_image_path, scale_factor)
-            base_image = read_image(base_image_path)
-        except Exception as e:
-            print(f"Error upscaling image: {e}")
-            base_image = read_image(image_path)
-    else:
-        base_image = read_image(image_path)
-
-    # read fits image
+    # read fits image and preprocess
     fits_image = open_fits_image(fits_path, 0)
+    prepped_fits = preprocess(fits_image)
 
-    # threshold it by tiling. play around with the tile size
-    processed_image = preprocess(base_image)
-    print(f"Thresholding {processed_image.shape[0]}x{processed_image.shape[1]} image with tile size {tile_size}...")
-    thresholded = thresh_by_tile(processed_image, tile_size=tile_size)
+    if image_path:
+        image = ski.io.imread(image_path, as_gray=True)
+        prepped_image = preprocess(image)
+    else:
+        prepped_image = prepped_fits
+
+    regions = find_rois(prepped_fits)
+    labeled_regions, num_regions = ndi.label(regions)
+
+    locs = ndi.find_objects(labeled_regions)
+    segmentation = np.zeros_like(fits_image)
+    for loc in locs:
+        img = prepped_image[loc]
+        clustered = kmeans(img, K=5, blur_strength=1)
+        cleared = clear_bg(clustered, bwidth=10, bg_min_count=50)
+        binarized = binarize_features(cleared, feature=feature)
+        segmentation[loc] = binarized
 
     # label image
     print("Labeling...")
-    label_image = find_objects(thresholded)
-    print(f"Found {label_image.max()} objects in image.")
+    label_image = ski.measure.label(segmentation)
+    print(f"Found {label_image.max()} {feature} in {num_regions} regions of interest.")
 
     # GET FOLLOWING REGION PROPERTIES FROM THESE LABELS
     '''bounding box, area, centroid coords, centroid pixel, intensity, minimum intensity, coords of min intensity 
     pixel, average intensity'''
 
     print("Getting region properties...")
-    props = measure.regionprops(label_image, intensity_image=processed_image)
+    props = ski.measure.regionprops(label_image, intensity_image=fits_image)
     print("Writing properties to file...")
     with open('output/region_properties_' + date + '_.csv', 'w') as file:
         file.write(
-            "Region Label, BBox Min Y, BBox Min X, BBox Max Y, BBox Max X, Area, Centroid Y, Centroid X, "
-            "Centroid Intensity, Min Intensity, Min Intensity Y, Min Intensity X, Avg Intensity\n"
+            "Region Label,BBox Min Y,BBox Min X,BBox Max Y,BBox Max X,Area,Centroid Y,Centroid X,"
+            "Centroid Intensity,Min Intensity,Min Intensity Y,Min Intensity X,Avg Intensity\n"
         )
         for region in props:
-            # Adjust bounding box and centroid by scale factor
-            bbox = tuple([int(b / scale_factor) for b in region.bbox])
-            centroid = tuple([int(c / scale_factor) for c in region.centroid])
-            area = int(region.area / (scale_factor ** 2))  # Area scales with the square of the scale factor
+            bbox = tuple([int(b) for b in region.bbox])
+            centroid = tuple([int(c) for c in region.centroid])
+            area = int(region.area) 
 
             # Get intensity data directly from the fits_image using adjusted coordinates
             centroid_intensity = fits_image[centroid]
@@ -223,17 +311,17 @@ def segment_core(image_path, fits_path, scale_factor=4, tile_size=2048):
             file.write(region_data)
 
     # rescale image intensity to 8-bit dtype so it can be overlaid with labels
-    rescaled_image = ski.exposure.rescale_intensity(processed_image)
+    rescaled_image = ski.exposure.rescale_intensity(fits_image)
     base_as_ubyte = ski.util.img_as_ubyte(rescaled_image)
     print("Plotting features on base image...")
     image_label_overlay = ski.color.label2rgb(label_image, image=base_as_ubyte)
 
 
     # output images from each step
-    print("Saving thresholded image...")
-    ski.io.imsave('output/thresholded_' + date + '_.png', ski.util.img_as_ubyte(thresholded))
+    print("Saving segmented image...")
+    ski.io.imsave('output/segmented_' + date + '_.png', ski.util.img_as_ubyte(segmentation.astype('bool')), check_contrast=False)
     print("Saving labeled image...")
-    ski.io.imsave('output/labeled_' + date + '_.png', ski.util.img_as_ubyte(image_label_overlay))
+    ski.io.imsave('output/labeled_' + date + '_.png', ski.util.img_as_ubyte(image_label_overlay), check_contrast=False)
     print("Done!")
 
     # remove upscaled image
@@ -241,5 +329,4 @@ def segment_core(image_path, fits_path, scale_factor=4, tile_size=2048):
 
 
 if __name__ == '__main__':
-    segment_core("test_res/hmi.in_45s.20150508_000000_TAI.2.continuum.fits.png", "test_res/hmi.in_45s.20150508_000000_TAI.2.continuum.fits", scale_factor=2)
-    # segment_core("upscaled_images/dn01/hmi.in_45s.20150508_000000_TAI.2.continuum.fits_out.png", "test_res/hmi.in_45s.20150508_000000_TAI.2.continuum.fits", scale_factor=2, tile_size=512)
+    segment_core("test_res/hmi.in_45s.20150508_000000_TAI.2.continuum.fits", feature='penumbrae')
